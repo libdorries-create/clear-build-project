71d9a09391c77f3f8e4d08e78a6ea686b1898087
libdorries-create <libdorries@icloud.com>
2026-07-15 18:32:22 +1000

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -248,8 +248,10 @@ class EnterpriseValidatorApp:
                 if pct > 0:
                     data.append([Paragraph(school, body_style), Paragraph(f"{pct:.1f}%", body_style)])
                 
-            t_box = Table(data, colWidths=)
+                        # Setting explicit dimensions for your description profile and score columns
+            t_box = Table(data, colWidths=[320, 80])
             t_box.setStyle(TableStyle([
+
                 ('BACKGROUND', (0, 0), (1, 0), colors.HexColor(t["accent"])),
                 ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
                 ('BOTTOMPADDING', (0, 0), (-1, 0), 6),
